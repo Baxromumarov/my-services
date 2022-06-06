a2755cb094db3d5b900a1b8e4a1f480259985ef0
Baxromumarov <baxromumarov10@gmail.com>
2022-06-06 09:09:05 +0500

    first commit

---
+++ api-gateway/api/handlers/v1/handler.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"github.com/baxromumarov/api-gateway/config"
+	"github.com/baxromumarov/api-gateway/pkg/logger"
+	"github.com/baxromumarov/api-gateway/services"
+	"github.com/baxromumarov/homework/write-template/pkg/logger"
+)
+
+type handlerV1 struct{
+	log logger.Logger
+	serviceManager services.IServiceManager
+	cfg config.Config
+}
+
+// HandlerV1Config ...
+type HandlerV1Config struct {
+	Logger logger.Logger
+	ServiceManager services.IServiceManager
+	Cfg config.Config
+}
+
+// New ...
+func New(c *HandlerV1Config) *handlerV1 {
+	return &handlerV1{
+		log: c.Logger,
+		serviceManager: c.ServiceManager,
+		cfg: c.Cfg,
+	}
+}
